f256d99eb611fbdd8c29a8f8cae894aaaa372be4
Alex-Joe <alex.ribeiro.souza@gmail.com>
2020-11-24 20:11:43 -0300

    html, add as divs com cada carta, css, add estilos efeito de transição e uma carta sobre a outra, primeiros comandos JS.

---
+++ content/script.js
@@ -0,0 +1,9 @@
+const cards = document.querySelectorAll('.cards');
+
+function flipCard() {
+	this.classList.toggle('flip');
+}
+
+cards.forEach((card) => {
+	card.addEventListener('click', flipCard)
+});
